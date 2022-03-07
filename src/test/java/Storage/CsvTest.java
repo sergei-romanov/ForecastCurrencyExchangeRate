c26897eeed96bba3511ce58f12f1ee0144689cea
Sergey Romanov <romanov.java@gmail.com>
2022-03-07 18:52:28 +0300

    update class CsvTest

--- src/test/java/Storage/CsvTest.java
+++ src/test/java/Storage/CsvTest.java
@@ -1,9 +1,11 @@
 package Storage;
 
+import model.Rate;
 import org.junit.jupiter.api.Test;
 import storage.Csv;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,13 +17,22 @@ class CsvTest {
     void parseCsvFromTable() {
         var path = Path.of("src/test/resources/CsvHelper.txt");
         var csv = Csv.createCsv(path);
-        var actual = csv.getTable();
-        var expect = List.of(List.of("01.02.2022", "86,5032", "Евро"),
-                List.of("29.01.2022", "86,6419", "Евро"),
-                List.of("28.01.2022", "88,4680", "Евро"));
-        for (int i = 0; i < actual.size(); i++) {
-            assertThat(actual.get(i)).isEqualTo(expect.get(i));
-        }
+        var table = csv.getTable();
+        List<String> actual = new ArrayList<>();
+       table.stream().map(Rate::toString).forEach(actual::add);
+        var expect = List.of(
+                "1 05.03.2022 7.44 TRY",
+                "1 04.03.2022 7.90 TRY",
+                "1 03.03.2022 7.36 TRY",
+                "1 02.03.2022 6.61 TRY",
+                "1 01.03.2022 6.77 TRY",
+                "1 28.02.2022 5.94 TRY",
+                "1 27.02.2022 5.94 TRY",
+                "1 26.02.2022 5.94 TRY",
+                "1 25.02.2022 6.15 TRY",
+                "1 24.02.2022 5.84 TRY",
+                "1 23.02.2022 5.84 TRY");
+        assertThat(actual).isEqualTo(expect);
     }
 
     @Test
@@ -29,7 +40,7 @@ class CsvTest {
         var path = Path.of("src/test/resources/CsvHelper.txt");
         var csv = Csv.createCsv(path);
         var actual = csv.getHeader();
-        String[] expect = {"data", "curs", "cdx"};
+        String[] expect = {"nominal", "data", "curs", "cdx"};
         assertThat(actual).isEqualTo(expect);
     }
 }
