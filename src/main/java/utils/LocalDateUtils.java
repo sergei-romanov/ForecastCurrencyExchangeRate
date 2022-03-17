749916f520ff9139575f124ab08431ecccf57a02
Sergey Romanov <romanov.java@gmail.com>
2022-03-17 07:10:31 +0300

    add class LocalDateUtils

---
+++ src/main/java/utils/LocalDateUtils.java
@@ -0,0 +1,7 @@
+package utils;
+
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateUtils {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+}
