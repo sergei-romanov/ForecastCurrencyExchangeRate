f34d2e9f276009bd44b1d6857785e873253ca6a0
Sergey Romanov <romanov.java@gmail.com>
2022-03-17 06:35:47 +0300

    add abstract class Algorithm

--- src/main/java/algorithms/Algorithm.java
+++ src/main/java/algorithms/Algorithm.java
@@ -1,22 +1,51 @@
 package algorithms;
 
-import storage.Csv;
+import model.Currency;
+import model.Rate;
+import storage.Storage;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
 
-public enum Algorithm {
-    ARITHMETIC_MEAN("ARITHMETIC_MEAN");
+import static java.time.temporal.ChronoUnit.DAYS;
 
-    final String name;
+public abstract class Algorithm {
+    private final Storage storage;
 
-    Algorithm(String name) {
-        this.name = name;
+    public Algorithm(Storage storage) {
+        this.storage = storage;
     }
 
-    public List<Double> getMethod(Csv csv, Period period) {
-        Algorithms algorithm = switch (this){
-            case ARITHMETIC_MEAN -> new ArithmeticMean();
-        };
-        return algorithm.calculating(csv, period);
+    public Rate someDayRate(LocalDate date, Currency currency) {
+        BigDecimal course = doSomeDayRate(date, storage.getTable().get(currency).getTable());
+        return new Rate.Builder()
+                .withNominal(1)
+                .withDate(date)
+                .withCourse(course)
+                .withCurrency(currency)
+                .build();
+    }
+
+    protected abstract BigDecimal doSomeDayRate(LocalDate date, Map<LocalDate, Rate> table);
+
+    public List<Rate> weekRate(LocalDate date, Currency currency) {
+        List<Rate> weekRate = new ArrayList<>();
+        IntStream.range(1, 8)
+                .mapToObj(day -> someDayRate(date.plusDays(day), currency))
+                .forEach(weekRate::add);
+        return weekRate;
+    }
+
+    public List<Rate> monthRate(LocalDate start, LocalDate end, Currency currency) {
+        List<Rate> monthRate = new ArrayList<>();
+        var countDayForEndMonth = (int) DAYS.between(start, end);
+        IntStream.range(1, countDayForEndMonth + 1)
+                .mapToObj(day -> someDayRate(start.plusDays(day), currency))
+                .forEach(monthRate::add);
+        return monthRate;
     }
 }
