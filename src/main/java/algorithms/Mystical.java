4a8a76f1add641be96362f2545cbc2e759e8be9d
Sergey Romanov <romanov.java@gmail.com>
2022-03-19 01:30:34 +0300

    fix bug class Mystical

--- src/main/java/algorithms/Mystical.java
+++ src/main/java/algorithms/Mystical.java
@@ -16,7 +16,6 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.time.temporal.ChronoUnit.DAYS;
-
 /**
  * Алгоритм “Мистический”
  * - Для расчета на дату используем среднее арифметическое из трех последних от этой даты полнолуний.
@@ -25,6 +24,7 @@ import static java.time.temporal.ChronoUnit.DAYS;
  * случайное число от -10% до +10% от значения предыдущей даты.
  */
 public class Mystical extends Algorithm {
+    private final static LocalDate LAST_DATE_WITH_CURRENT_DATA = LocalDate.of(2022,3,5);
     private static final Logger LOG = LoggerFactory.getLogger(Mystical.class);
 
     public Mystical(Storage storage) {
@@ -80,9 +80,8 @@ public class Mystical extends Algorithm {
         return min.add(range.multiply(BigDecimal.valueOf(Math.random())));
     }
 
-    private List<LocalDate> getLastThreeMoonPhase() {
-        LocalDate dateNow = LocalDate.now();
-        LocalDate date = dateNow.minusYears(1);
+    private static List<LocalDate> getLastThreeMoonPhase() {
+        LocalDate date = LAST_DATE_WITH_CURRENT_DATA.minusYears(2);
         List<LocalDate> moon = new ArrayList<>();
         MoonPhase.Parameters parameters = MoonPhase.compute()
                 .phase(MoonPhase.Phase.FULL_MOON);
@@ -93,9 +92,7 @@ public class Mystical extends Algorithm {
             LocalDate nextFullMoon = moonPhase
                     .getTime()
                     .toLocalDate();
-            if (nextFullMoon.getYear() == dateNow.getYear()
-                    && nextFullMoon.getMonth() == dateNow.getMonth()
-                    && nextFullMoon.getDayOfMonth() > dateNow.getDayOfMonth()) {
+            if (nextFullMoon.isAfter(LAST_DATE_WITH_CURRENT_DATA)) {
                 break;
             }
             moon.add(nextFullMoon);
@@ -105,5 +102,4 @@ public class Mystical extends Algorithm {
                 .skip(moon.size() - 3)
                 .collect(Collectors.toList());
     }
-
 }
